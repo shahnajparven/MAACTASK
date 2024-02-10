9a177288382c8151d9ebc1bf9041b7c25b1bdb24
shahnaj parven <shahnajritu21@gmail.com>
2024-02-10 21:47:46 +0600

    chamge

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { Suspense } from "react";
+
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Spinner from "./component/Spinner";
-
 import UserList from "./pages/user/UserList";
 import Navbar from "./component/Navbar";
 import UserDetails from "./pages/user/UserDetails";
@@ -12,14 +10,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <Suspense fallback={<Spinner />}>
         <Routes>
           <Route index element={<Home />} />
           <Route path={"userlist"} element={<UserList />} />
           <Route path={"userlist/:id"} element={<UserDetails />} />
           <Route path={"adduser"} element={<AddUser />} />
         </Routes>
-      </Suspense>
     </>
   );
 }
